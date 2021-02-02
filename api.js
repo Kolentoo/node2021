12af06fce1576e889f2f05dacdee1e444e1a388a
LXQ <515835543@qq.com>
2021-02-02 21:47:34 +0800

    修改爬虫配置和时间
    Signed-off-by: LXQ <515835543@qq.com>

--- api.js
+++ api.js
@@ -48,6 +48,7 @@ const schedule = require('node-schedule');
 
   const browser = await puppeteer.launch({
       headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
 
@@ -55,7 +56,7 @@ const schedule = require('node-schedule');
     // await page.goto('https://movie.douban.com/tv/#!type=tv&tag=%E6%97%A5%E6%9C%AC%E5%8A%A8%E7%94%BB&sort=recommend&page_limit=20&page_start=0');
     // 全部动漫列表
   // 每天0点获取最新动漫信息列表
-  schedule.scheduleJob('0 45 17 * * *',async()=>{
+  schedule.scheduleJob('0 49 21 * * *',async()=>{
     console.log('开始执行定时任务');
     await page.goto('https://movie.douban.com/tag/#/?sort=U&range=0,10&tags=%E5%8A%A8%E6%BC%AB');
     await page.setViewport({
@@ -69,7 +70,7 @@ const schedule = require('node-schedule');
     
     await page.click('.th-list');
     let getItems = setInterval(async()=>{
-      if(times<=50){
+      if(times<=0){
         await page.click('.more');
         console.log('加载更多');
         times++;
