ca6e97d72f53d16c6d3a97f6c5b2360b443318ba
Kolento <515835543@qq.com>
2021-02-04 11:12:21 +0800

    fixed example
    Signed-off-by: Kolento <515835543@qq.com>

--- example.js
+++ example.js
@@ -1,12 +1,13 @@
 const puppeteer = require('puppeteer');
-console.log('开始执行')
-(async () => {
-const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-const page = await browser.newPage();
-await page.goto('https://example.com');
-await page.screenshot({path: 'example.png'});
 
-console.log('截图完成')
-await browser.close();
-console.log('浏览器已完毕')
+(async () => {
+  console.log('开始执行')
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await page.screenshot({path: 'example.png'});
+
+  console.log('截图完成')
+  await browser.close();
+  console.log('浏览器已完毕')
 })();
